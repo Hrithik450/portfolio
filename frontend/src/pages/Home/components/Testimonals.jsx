b98d6ae73c66ffe57b4d9f793a0b4eaf15f49f97
Hruthik <mhrithik450@gmail.com>
2025-01-12 00:56:38 +0530

    34thcommit

--- frontend/src/pages/Home/components/Testimonals.jsx
+++ frontend/src/pages/Home/components/Testimonals.jsx
@@ -70,7 +70,7 @@ const Testimonals = () => {
           </SectionTitle>
           <Description>
             Here is some love words from my clients around the world. Hope you
-            will here next.
+            will be here next.
           </Description>
         </div>
 
