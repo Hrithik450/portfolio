4d4bacfaf68ffa2a31e7ab7e3a4e45dcd2488a27
Hruthik <mhrithik450@gmail.com>
2025-01-16 17:57:38 +0530

    59thcommit

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -7,7 +7,7 @@ export const generateTokenandSetcookie = (res, userID) => {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
+    secure: true,
     sameSite: "None",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
