87e95a50cd2ce3d731c2ac7fdab7150dce41d8ab
Hruthik <mhrithik450@gmail.com>
2025-01-19 16:48:55 +0530

    111thcommit

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -6,11 +6,12 @@ export const generateTokenandSetcookie = (res, userID) => {
   });
 
   res.cookie("token", token, {
-    httpOnly: true,
+    httpOnly: false,
     secure: true,
     sameSite: "None",
     path: "/",
     expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+    domain: ".onrender.com",
   });
 
   return token;
